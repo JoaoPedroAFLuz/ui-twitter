e80d6d41d673c696364dfffb7fcc0f8b3bd31dcc
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-02-12 00:49:23 -0300

    feat: sidebar component

--- src/main.tsx
+++ src/main.tsx
@@ -1,74 +1,16 @@
+import { Sparkle } from 'phosphor-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import {
-  Bell,
-  BookmarkSimple,
-  DotsThreeCircle,
-  Envelope,
-  FileText,
-  Hash,
-  House,
-  Sparkle,
-  User,
-} from 'phosphor-react';
-
-import twitterLogo from './assets/logo-twitter.svg';
-import './global.css';
 import { Tweet } from './components/Tweet';
+import { Sidebar } from './components/Sidebar';
+
+import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar">
-        <img className="logo" src={twitterLogo} alt="Logo" />
-
-        <nav className="main-navigation">
-          <a className="active" href="">
-            <House weight="fill" />
-            Home
-          </a>
-
-          <a href="">
-            <Hash />
-            Explore
-          </a>
-
-          <a href="">
-            <Bell />
-            Notifications
-          </a>
-
-          <a href="">
-            <Envelope />
-            Messages
-          </a>
-
-          <a href="">
-            <BookmarkSimple />
-            Bookmarks
-          </a>
-
-          <a href="">
-            <FileText />
-            Lists
-          </a>
-
-          <a href="">
-            <User />
-            Profile
-          </a>
-
-          <a href="">
-            <DotsThreeCircle />
-            More
-          </a>
-        </nav>
-
-        <button className="new-tweet" type="button">
-          Tweet
-        </button>
-      </aside>
+      <Sidebar />
 
       <div className="content">
         <main className="timeline">
@@ -92,6 +34,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 
           <div className="separator" />
 
+          <Tweet />
+          <Tweet />
+          <Tweet />
           <Tweet />
           <Tweet />
           <Tweet />
