f02ebc5784b05e0ff01d29e813597e38b5f964b2
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-02-12 11:22:14 -0300

    feat: create a new tweet

--- src/pages/Timeline/index.tsx
+++ src/pages/Timeline/index.tsx
@@ -1,28 +1,43 @@
+import { FormEvent, useState } from 'react';
+
 import { Header } from '../../components/Header';
 import { Separator } from '../../components/Separator';
 import { Tweet } from '../../components/Tweet';
 
 import './styles.css';
 
-const tweets = [
-  'Meu terceiro tweet',
-  'Meu segundo tweet',
-  'Meu primeiro tweet',
-];
-
 export function Timeline() {
+  const [newTweet, setNewTweet] = useState('');
+  const [tweets, setTweets] = useState([
+    'Meu terceiro tweet',
+    'Meu segundo tweet',
+    'Meu primeiro tweet',
+  ]);
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault();
+
+    setTweets((prevState) => [newTweet, ...prevState]);
+    setNewTweet('');
+  }
+
   return (
     <main className="timeline">
       <Header title="Home" />
 
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img
             src="https://github.com/JoaoPedroLuz57.png"
             alt="João Pedro Luz"
           />
 
-          <textarea id="tweet" placeholder="What's happening?"></textarea>
+          <textarea
+            id="tweet"
+            placeholder="What's happening?"
+            value={newTweet}
+            onChange={(event) => setNewTweet(event.target.value)}
+          ></textarea>
         </label>
 
         <button type="submit">Tweet</button>
